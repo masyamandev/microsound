01a6808ff131ebd50cf50ca31176074b7668eae0
Aleksandr Maksymenko <amaksymenko@cloudmade.com>
2019-07-30 19:55:18 +0300

    Initial implementation of 16x2 display demo

---
+++ demos/atmega8lcd/main.c
@@ -0,0 +1,177 @@
+#include <inttypes.h>
+#include <avr/io.h>
+//#include <avr/iom8.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+
+#ifndef F_CPU
+#define F_CPU 8000000
+#endif
+
+#define SAMPLE_PERCUSSION	0
+#define SAMPLE_PIANO		1
+#define SAMPLE_HARMONICA	2
+#define SAMPLE_BASS			3
+#define SAMPLE_OVERDRIVE	4
+#define SAMPLE_MUSICBOX		5
+
+//#define BUFFER_BITS	16
+#define BUFFER_SIZE 64
+#define MICROSOUND_FREQUENCY_DIVIDER	4
+//#define INTERPOLATION_STRENGTH	2
+//#define INTERPOLATE_WAVE
+#define CHANNELS_SIZE	5
+#define SAMPLES_SIZE	16
+#define USE_NOISE_CHANNEL
+
+void fillBufferAtLeastMs(uint8_t delay);
+//#define _delay_ms	fillBufferAtLeastMs
+
+
+#include "../../microsound/devices/atmega8timer2cpu8mhz.h"
+#include "../../microsound/micromusic.h"
+
+#include "../../microsound/instruments/piano.h"
+#include "../../microsound/instruments/bassGuitar.h"
+//#include "../../microsound/instruments/accordion.h"
+#include "../../microsound/instruments/harmonica.h"
+#include "../../microsound/instruments/overdrivenGuitarChord.h"
+#include "../../microsound/instruments/percussion.h"
+#include "../../microsound/instruments/musicbox.h"
+
+
+//#include "../../microsound/samples/singlechannel.h"
+#include "../../microsound/samples/oh_susanna.h"
+#include "../../microsound/samples/for_elise.h"
+
+#include "../../lcd1602/lcd1602.h"
+
+const uint8_t PIANO_CHAR_A[] PROGMEM = {
+		0b10101,
+		0b10101,
+		0b10101,
+		0b10101,
+		0b10001,
+		0b11011,
+		0b11011,
+		0b11011
+};
+const uint8_t PIANO_CHAR_B[] PROGMEM = {
+		0b10101,
+		0b10101,
+		0b10101,
+		0b10101,
+		0b00101,
+		0b01101,
+		0b01101,
+		0b01101
+};
+const uint8_t PIANO_CHAR_C[] PROGMEM = {
+		0b10101,
+		0b10101,
+		0b10101,
+		0b10101,
+		0b00100,
+		0b01110,
+		0b01110,
+		0b01110
+};
+
+
+void fillBufferAtLeastMs(uint8_t delay) {
+	fillMusicBuffer();
+}
+
+#define CUSTOMM_CHARS	4
+
+int main(void)
+{
+	initLCD();
+
+	initCharProgmem(5, PIANO_CHAR_A);
+	initCharProgmem(6, PIANO_CHAR_B);
+	initCharProgmem(7, PIANO_CHAR_C);
+	cursor(1, 0);
+	uint8_t i;
+	for(i = 0; i < 5; i++) {
+		printChar(5);
+		printChar(6);
+		printChar(7);
+		printChar(5);
+	}
+
+	initMusic();
+	setSample(SAMPLE_PERCUSSION, playPercussion);
+	setSample(SAMPLE_PIANO, playPiano);
+	setSample(SAMPLE_HARMONICA, playHarmonica);
+	setSample(SAMPLE_BASS, playBassGuitar);
+	setSample(SAMPLE_OVERDRIVE, playOverdrivenGuitarChord);
+	setSample(SAMPLE_MUSICBOX, playMusicbox);
+
+	sei();
+
+	while (1) {
+
+		fillBufferAtLeastMs(0);
+
+		cursor(0, 0);
+
+//		fillBufferAtLeastMs(0);
+
+		uint8_t pos;
+		uint8_t currentChar = 0;
+		static uint8_t customCharsPos[CUSTOMM_CHARS];
+
+		for (pos = 0; pos < 16; pos++) {
+			uint8_t note1 = 0;
+			uint8_t note2 = 0;
+			uint8_t note3 = 0;
+			for (i = 0; i < CHANNELS_SIZE; i++) {
+				if (channels[i]->waveStep == pgm_read_word(&frequencies[NOTE_C2 + pos * 3])) {
+					note1 += channels[i]->currentVolume;
+				}
+				if (channels[i]->waveStep == pgm_read_word(&frequencies[NOTE_C2 + pos * 3 + 1])) {
+					note2 += channels[i]->currentVolume;
+				}
+				if (channels[i]->waveStep == pgm_read_word(&frequencies[NOTE_C2 + pos * 3 + 2])) {
+					note3 += channels[i]->currentVolume;
+				}
+			}
+
+			if (note1 + note2 + note3 > 0) {
+				static uint8_t customChar[8];
+				uint8_t k;
+				for (k = 0; k < 8; k++) {
+					customChar[7 - k] = ((note1 > (k << 3)) ? 0b10000 : 0) |
+							((note2 > (k << 3)) ? 0b00100 : 0) |
+							((note3 > (k << 3)) ? 0b00001 : 0);
+				}
+				fillBufferAtLeastMs(0);
+
+				if (customCharsPos[currentChar] != pos && customCharsPos[currentChar] < 16) {
+					cursor(0, customCharsPos[currentChar]);
+					printChar(' ');
+					customCharsPos[currentChar] = pos;
+				}
+				cursor(0, pos);
+
+				printChar(currentChar);
+				initChar(currentChar, &customChar);
+				currentChar++;
+				if (currentChar >= CUSTOMM_CHARS) {
+					currentChar = 0;
+				}
+			} else {
+				printChar(' ');
+			}
+
+			fillBufferAtLeastMs(0);
+		}
+
+
+		if (isMusicStopped) {
+			playMusic(forElise);
+		}
+
+	}
+}
---
+++ lcd1602/lcd1602.h
@@ -0,0 +1,133 @@
+#define LCD_DDR 	DDRD
+#define LCD_PORT	PORTD
+#define LCD_PIN_D4		0
+#define LCD_PIN_RS		5
+#define LCD_PIN_EN		6
+
+#define LCD_PIN_DATA_MASK	((1 << LCD_PIN_D4) | (1 << (LCD_PIN_D4 + 1)) | (1 << (LCD_PIN_D4 + 2)) | (1 << (LCD_PIN_D4 + 3)))
+#define LCD_PIN_MASK		(LCD_PIN_DATA_MASK | (1 << LCD_PIN_EN) | (1 << LCD_PIN_RS))
+
+#include <avr/delay.h>
+
+
+inline void __lcd_delay() {
+//	fillBufferAtLeastMs(0);
+	_delay_ms(0.05);
+//	fillBufferAtLeastMs(0);
+}
+
+inline void __lcd_longDelay() {
+//	fillBufferAtLeastMs(0);
+//	_delay_ms(1.1);
+	_delay_ms(4.1);
+//	fillBufferAtLeastMs(0);
+}
+
+inline void __lcd_initDelay() {
+	_delay_ms(15);
+}
+
+
+
+inline void pulseLcd()
+{
+//    __lcd_delay(); // TODO remove?
+//    LCD_PORT &= ~(1 << LCD_PIN_EN);
+//    __lcd_delay(); // TODO remove?
+    LCD_PORT |= (1 << LCD_PIN_EN);
+//    __lcd_delay(); // TODO remove?
+    LCD_PORT &= ~(1 << LCD_PIN_EN);
+    __lcd_delay();
+}
+
+void sendByte(uint8_t byteToSend, uint8_t isData)
+{
+	uint8_t clear = ~LCD_PIN_MASK;
+	uint8_t dataBit = 0;
+	if (isData) {
+		clear |= (1 << LCD_PIN_RS);
+		dataBit |= (1 << LCD_PIN_RS);
+	}
+
+    LCD_PORT &= clear;
+    LCD_PORT |= ((byteToSend >> 4) << LCD_PIN_D4) | dataBit;
+    pulseLcd();
+
+    LCD_PORT &= clear;
+    LCD_PORT |= ((byteToSend & 0x0F) << LCD_PIN_D4) | dataBit;
+    pulseLcd();
+}
+
+void cursor(uint8_t row, uint8_t col)
+{
+	uint8_t address;
+    if (row == 0) {
+        address = 0;
+    } else {
+        address = 0x40;
+    }
+
+    address |= col;
+    sendByte(0x80 | address, 0);
+//    __lcd_longDelay();
+}
+
+void clearLcdScreen()
+{
+    sendByte(0x01, 0);
+    __lcd_longDelay();
+    sendByte(0x02, 0);
+    __lcd_longDelay();
+}
+
+inline void initLCD() {
+
+	LCD_DDR |= LCD_PIN_MASK;
+
+    __lcd_initDelay();
+
+	// Send 3 times 8-bit mode and 1 time 4-bit mode
+	sendByte(0b00110011, 0);
+    sendByte(0b00110010, 0);
+
+    // Init rest of settings in 4-bit mode
+    sendByte(0b00101000, 0);
+    sendByte(0b00001100, 0);
+    sendByte(0b00000110, 0);
+
+    clearLcdScreen();
+}
+
+void initChar(uint8_t c, uint8_t* tile)
+{
+    sendByte(0b01000000 | (c << 3), 0);
+    for (int i = 0; i < 8; i++) {
+        sendByte(*tile, 1);
+        tile++;
+    }
+}
+
+void initCharProgmem(uint8_t c, uint8_t* tile)
+{
+    sendByte(0b01000000 | (c << 3), 0);
+    for (int i = 0; i < 8; i++) {
+        sendByte(pgm_read_byte(tile), 1);
+        tile++;
+    }
+}
+
+inline void printChar(char c)
+{
+	sendByte(c, 1);
+}
+
+// TODO add progmem?
+void printStr(char *text)
+{
+    char *c;
+    c = text;
+    while ((c != 0) && (*c != 0)) {
+        sendByte(*c, 1);
+        c++;
+    }
+}
